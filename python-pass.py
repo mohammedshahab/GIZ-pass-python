ee44abd1c77478c1ab39b9f130c791cbeeef1d46
mohammedshahab <iraqapple435@gmail.com>
2021-09-30 21:53:41 +0300

    upolad task

--- python-pass.py
+++ python-pass.py
@@ -1,7 +1,18 @@
-
-
 class Solution:
 
     @staticmethod
     def longest_palindromic(s: str) -> str:
-        pass
+        m = ''
+        if len(s) == 1:
+            return s
+
+        for i in range(len(s)):
+            for j in range(len(s) + 1):
+                if len(m) >= j - i:
+                    continue
+                elif s[i:j] == s[i:j][::-1]:
+                    m = s[i:j]
+                    continue
+
+        return m
+print(Solution.longest_palindromic("babad"))
